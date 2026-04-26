411c6522ce1144e565512a4323a370a8fe495033
Hugo <hudela@student.ethz.ch>
2026-04-26 20:15:56 +0200

    Increase duck_margin to 0.08 to keep slightly more hand pixels

--- run_job_postprocess_hand_depth.sh
+++ run_job_postprocess_hand_depth.sh
@@ -15,6 +15,6 @@ python postprocess_hand_masks_depth.py \
     --pose_dir      /work/courses/3dv/team22/foundationpose/debug/duck_vda_palm_rot/ob_in_cam \
     --depth_dir     /work/courses/3dv/team22/foundationpose/data/20250804_104715/depth \
     --out_dir       /work/courses/3dv/team22/foundationpose/data/20250804_104715/masks_hand_final \
-    --duck_margin   0.05 \
+    --duck_margin   0.08 \
     --min_depth     0.2 \
     --depth_scale   0.37
